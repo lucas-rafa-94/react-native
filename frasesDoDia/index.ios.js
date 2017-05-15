bdc59b98c1e9696a0128ab28c0e07a0aba5f5300
Lucas Rafael dos Santos <lucas_rafa_94@hotmail.com>
2017-05-15 01:13:07 -0300

    commit frases do Dia ok

--- frasesDoDia/index.ios.js
+++ frasesDoDia/index.ios.js
@@ -9,45 +9,82 @@ import {
   AppRegistry,
   StyleSheet,
   Text,
-  View
+  View,
+  Image,
+  TouchableOpacity
 } from 'react-native';
 
-export default class frasesDoDia extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
-      </View>
-    );
+const Estilos = {
+
+  principal:{
+    flex: 1
+  },
+
+  cabecalho:{
+    flex: 2
+  },
+
+  conteudo: {
+    flex:8,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  rodape:{
+    flex: 2
+  },
+  estiloBotao:{
+    marginTop: 40,
+    backgroundColor: 'green',
+    width: 200,
+    height: 40,
+    borderRadius: 10,
+    padding:10
+  },
+  estiloTextoBotao:{
+    color: '#fff',
+    textAlign: 'center',
   }
+
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
+const funcFrase = () =>{
+  var random = Math.random();
+  var numeroEscolhido = Math.floor(random * 5);
 
-AppRegistry.registerComponent('frasesDoDia', () => frasesDoDia);
+  var frases = Array();
+
+  frases[0] = 'Frase 1';
+  frases[1] = 'Frase 2';
+  frases[2] = 'Frase 3';
+  frases[3] = 'Frase 4';
+  frases[4] = 'Frase 5';
+
+  return alert(frases[numeroEscolhido])
+}
+
+const App = () => {
+
+  const {principal, cabecalho, conteudo, rodape, estiloBotao, estiloTextoBotao} = Estilos;
+
+  return (
+    <View style={principal}>
+      <View style={cabecalho}>
+
+      </View>
+      <View style={conteudo}>
+          <Image source={require('./img/logo.png')}></Image>
+          <TouchableOpacity style={estiloBotao} onPress={funcFrase}>
+              <Text style={estiloTextoBotao}>Frase do dia</Text>
+          </TouchableOpacity>
+      </View>
+      <View style={rodape}>
+
+      </View>
+
+    </View>
+
+  );
+
+};
+
+AppRegistry.registerComponent('frasesDoDia', () => {return App});
